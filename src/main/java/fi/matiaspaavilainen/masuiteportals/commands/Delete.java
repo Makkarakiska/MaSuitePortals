76d36e26387558a15796f769b8afffcfcce994c9
Masa <masa@simasasolutions.com>
2018-09-18 19:07:13 +0300

    Added base

--- src/main/java/fi/matiaspaavilainen/masuiteportals/commands/Delete.java
+++ src/main/java/fi/matiaspaavilainen/masuiteportals/commands/Delete.java
@@ -1,6 +1,9 @@
 package fi.matiaspaavilainen.masuiteportals.commands;
 
+import fi.matiaspaavilainen.masuitecore.chat.Formator;
+import fi.matiaspaavilainen.masuitecore.config.Configuration;
 import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Command;
 
 public class Delete extends Command {
@@ -9,7 +12,17 @@ public class Delete extends Command {
     }
 
     @Override
-    public void execute(CommandSender commandSender, String[] strings) {
-
+    public void execute(CommandSender cs, String[] args) {
+        if(!(cs instanceof ProxiedPlayer)){
+            return;
+        }
+        Formator formator = new Formator();
+        Configuration config = new Configuration();
+        ProxiedPlayer p = (ProxiedPlayer) cs;
+        if(args.length == 1){
+            // Delete portal
+        }else{
+            formator.sendMessage(p, config.load("portals", "syntax.yml").getString("portal.delete"));
+        }
     }
 }
