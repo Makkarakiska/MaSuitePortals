a2da013b9f1f2b98a8351fc43a2f634215546af4
Matias Paavilainen <matias@paavilainen.net>
2020-01-11 23:12:49 +0200

    Added PortalCommand

---
+++ src/main/java/fi/matiaspaavilainen/masuiteportals/bukkit/commands/PortalCommand.java
@@ -0,0 +1,64 @@
+package fi.matiaspaavilainen.masuiteportals.bukkit.commands;
+
+import com.sk89q.worldedit.IncompleteRegionException;
+import com.sk89q.worldedit.LocalSession;
+import com.sk89q.worldedit.regions.Region;
+import fi.matiaspaavilainen.masuitecore.acf.BaseCommand;
+import fi.matiaspaavilainen.masuitecore.acf.annotation.*;
+import fi.matiaspaavilainen.masuitecore.core.channels.BukkitPluginChannel;
+import fi.matiaspaavilainen.masuiteportals.bukkit.MaSuitePortals;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+
+public class PortalCommand extends BaseCommand {
+
+    private MaSuitePortals plugin;
+
+    public PortalCommand(MaSuitePortals plugin) {
+        this.plugin = plugin;
+    }
+
+    @CommandAlias("setportal|createportal|portalset")
+    @CommandPermission("masuiteportals.portal.set")
+    @Description("Creates a new portal or updates existing portal")
+    @CommandCompletion("@portals warp|server * *")
+    public void setPortalCommand(Player player, @Single String name, @Single String type, @Single String destination, @Single String fillType) {
+
+        LocalSession localSession = plugin.we.getSession(player);
+        if (localSession == null) {
+            plugin.formator.sendMessage(player, plugin.config.load("portals", "messages.yml").getString("no-selected-area"));
+            return;
+        }
+        try {
+            Region rg = localSession.getSelection(localSession.getSelectionWorld());
+            if (rg == null) {
+                plugin.formator.sendMessage(player, plugin.config.load("portals", "messages.yml").getString("no-selected-area"));
+                return;
+            }
+            if (Material.matchMaterial(fillType.toUpperCase()) == null && !fillType.contains("nether_portal")) {
+                plugin.formator.sendMessage(player, plugin.config.load("portals", "messages.yml").getString("invalid-material"));
+                return;
+            }
+            String minLoc = player.getWorld().getName() + ":" + rg.getMinimumPoint().getBlockX() + ":" + rg.getMinimumPoint().getBlockY() + ":" + rg.getMinimumPoint().getBlockZ();
+            String maxLoc = player.getWorld().getName() + ":" + rg.getMaximumPoint().getX() + ":" + rg.getMaximumPoint().getY() + ":" + rg.getMaximumPoint().getZ();
+            new BukkitPluginChannel(plugin, player, "MaSuitePortals", "SetPortal", player.getName(), name, type, destination, fillType, minLoc, maxLoc).send();
+        } catch (IncompleteRegionException e) {
+            plugin.formator.sendMessage(player, plugin.config.load("portals", "messages.yml").getString("no-selected-area"));
+        }
+    }
+
+    @CommandAlias("delportal|deleteportal|portaldel")
+    @CommandPermission("masuiteportals.portal.delete")
+    @Description("Deletes existing portal")
+    @CommandCompletion("@portals")
+    public void deletePortalCommand(Player player, @Single String name) {
+        new BukkitPluginChannel(plugin, player, "MaSuitePortals", "DelPortal", player.getName(), name).send();
+    }
+
+    @CommandAlias("portals|portalslist|listportals")
+    @CommandPermission("masuiteportals.portal.delete")
+    @Description("List all portals")
+    public void listPortalsCommand(Player player) {
+        new BukkitPluginChannel(plugin, player, "MaSuitePortals", "ListCommand", player.getName()).send();
+    }
+}
